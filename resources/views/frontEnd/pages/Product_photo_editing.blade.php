531c8a8e6c6e6d8e87e1f62775b4867572756419
AzharRaihan <48883613+AzharRaihan@users.noreply.github.com>
2020-06-26 00:26:12 +0600

    product-photo-editting page responsive complite

--- resources/views/frontEnd/pages/Product_photo_editing.blade.php
+++ resources/views/frontEnd/pages/Product_photo_editing.blade.php
@@ -10,7 +10,7 @@
 <!--==** Service Section Start **==-->
 <section id="service">
     <div class="container py-col-2 py-sm-3 py-md-4 py-lg-5 py-xl-5">
-        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3">
+        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3 mobile-pt">
             <h1 class="pb-col-2 pb-sm-3 pb-md-4 pb-lg-5 pb-xl-5 text-center">Product Photo Editing Service</h1>
             <div class="box" style="box-shadow: 3px 3px 13px 2px rgba(0,0,0,.3);">
                 <img src="{{asset('assets/frontEnd/images/Service_All/Product_photo-editing_Service/Product_photo_editing_Service.png')}}"
@@ -27,7 +27,7 @@
 <!--==** Our Work Section Start **==-->
 <section id="out_work">
     <div class="container pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
-        <div class="head">
+        <div class="head mobile-pt">
             <h1 class="text-center text-uppercase">Our Work</h1>
         </div>
         <div class="box my-col-2 my-sm-3 my-md-4 my-lg-5 my-xl-5"
@@ -54,7 +54,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Photo Cut Out</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">Actually cut out images mean Product clean-up around the 
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">Actually cut out images mean Product clean-up around the 
                             edges. Simply cut out images is a soft Photoshop working. Products like T-shirt, 
                             Boots, Bags, Bottles, simple packaging products are in this sector. Where you need 
                             to pay us $.39 or less USD for simple cut out images. You may call cut out as Deep
@@ -70,9 +70,9 @@
             </div>
             <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
                 <div class="row pt-sm-4 pt-xs-3">
-                    <div class="col-md-6">
-                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Product Photo Resized</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">Product Background Crop & Resized of photos for different
+                    <div class="col-md-6 mobile-pt">
+                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4 mobile-pt"><b>Product Photo Resized</b></h4>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">Product Background Crop & Resized of photos for different
                             advantages to assure a professional-looking compatible with trade photos is imperative
                             owing to the fact that using in adjustable shaped to embed in a social post, printing or 
                             ecommerce projects. All desired necessities will be perfectly covered by using the 
@@ -86,9 +86,9 @@
             </div>
             <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
                 <div class="row pt-sm-4 pt-xs-3">
-                    <div class="col-md-6">
-                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Ecommerce Retouching& Color Adjust</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">An ecommerce image retouching & retooling facilitates to 
+                    <div class="col-md-6 mobile-pt">
+                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4 mobile-pt"><b>Ecommerce Retouching& Color Adjust</b></h4>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">An ecommerce image retouching & retooling facilitates to 
                             rebirth in different looking & color to be matched to the web stores.  Our service 
                             group is conscious of product fabric, multiply composition & pattern. Discount 
                             existence is all-time for huge volume product files.</p>
@@ -108,7 +108,7 @@
 <!--==** Image Editing Solution Section Start **==-->
 <div class="image_editing">
     <div class="container">
-        <div class="imge-article">
+        <div class="imge-article mobile-pl mobile-pr">
             <h1 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4">Ecommerce Product Editing – Its Acceptance</h1>
             <p>Ecommerce Product Editing Service where the retailers recruit ecommerce photographers to shoot
                  their products & make an effort vending them through on the web is a must to manufacture the 
@@ -116,13 +116,13 @@
                  resizing & shadow creation to fit for Amazon, eBay, and Shopify & load them on ecommerce webpage.
                 <br> 
             </p>
-            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2"><b>Ecommerce Product White Background for Growth</b></h5>
+            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2 mobile-pt"><b>Ecommerce Product White Background for Growth</b></h5>
             <p>The specialists of Ecommerce Solution highly recommend making your website beautiful & easy to approach. That is why ecommerce products looking a great way to achieve clients by developing it pure white background & here Clipping Path is first needed to draw.</p>
 
-            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2"><b>Retail Challenges by Ecommerce Product Editing </b></h5>
+            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2 mobile-pt"><b>Retail Challenges by Ecommerce Product Editing </b></h5>
             <p>First and foremost retail business & idea basically be decided by the product’s quality & its exhibition. The products containing pure white background on Amazon, Alibaba, Shopify, eBay & your webpage on where you show off your products succeed to augment sale.</p>
 
-            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2"><b>Ecommerce Product Pictures Resized, Shadow & Retouched - Designer Country</b></h5>
+            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2 mobile-pt"><b>Ecommerce Product Pictures Resized, Shadow & Retouched - Designer Country</b></h5>
             <p>Unbelievably speedy performance, fair-minded charge funds & remarkable time maintained are determined as our idea of service as our cordial supportive team is going on your project to maintain turnaround hours while you are sleeping.  
             Our Photoshop team settled in Bangladesh outsources images globally in low charged where quality & resolution high, big project files we edit with active buyer support by offering a free trial option. 
             Keep in mind us when you urgently need Background Removal, Retouch, Logo & Graphic Design, and Vector Conversation.
