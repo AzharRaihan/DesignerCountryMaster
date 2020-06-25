044e7683c9de943596c0b33ed8d0ef6c1284b449
AzharRaihan <48883613+AzharRaihan@users.noreply.github.com>
2020-06-25 20:22:06 +0600

    Ghost Mannequine-page responsive complite

--- resources/views/frontEnd/pages/ghosht_manneuquin.blade.php
+++ resources/views/frontEnd/pages/ghosht_manneuquin.blade.php
@@ -10,8 +10,8 @@
 <!--==** Service Section Start **==-->
 <section id="service">
     <div class="container py-col-2 py-sm-3 py-md-4 py-lg-5 py-xl-5">
-        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3">
-            <h1 class="pb-col-2 pb-sm-3 pb-md-4 pb-lg-5 pb-xl-5 text-center">Ghost Mannequin Service</h1>
+        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3 mobile-pt">
+            <h1 class="pb-col-2 pb-sm-3 pb-md-4 pb-lg-5 pb-xl-5  text-center">Ghost Mannequin Service</h1>
             <div class="box" style="box-shadow: 3px 3px 13px 2px rgba(0,0,0,.3);">
                 <img src="{{asset('assets/frontEnd/images/Service_All/Ghost_Mannequin_Service/Ghost_Mannequin_Service.png')}}"
                     alt="Ghost_Mannequin_Service" class="img-fluid" style="width: 100%;">
@@ -27,7 +27,7 @@
 <!--==** Our Work Section Start **==-->
 <section id="out_work">
     <div class="container pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
-        <div class="head">
+        <div class="head mobile-pt">
             <h1 class="text-center text-uppercase">Our Work</h1>
         </div>
         <div class="box my-col-2 my-sm-3 my-md-4 my-lg-5 my-xl-5"
@@ -54,7 +54,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Neck Joint on Ghost Mannequin</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">Dc Outsource multiple image procedures including retouch & background removal 
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">Dc Outsource multiple image procedures including retouch & background removal 
                             where product manipulated solution for ecommerce web loading you will get superbly to promote a brand.
                         </p>
                     </div>
@@ -66,9 +66,9 @@
             </div>
             <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
                 <div class="row pt-sm-4 pt-xs-3">
-                    <div class="col-md-6">
-                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Fashion Manipulation</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">Undeniably fashion Clothes when you shoot are insisted on 
+                    <div class="col-md-6 mobile-pt">
+                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4 mobile-pt"><b>Fashion Manipulation</b></h4>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">Undeniably fashion Clothes when you shoot are insisted on 
                         manipulated to point out the unpublished involvement; here designers ought to own 
                         thinking power for highlighting the part coordinated totally.</p>
                     </div>
@@ -80,9 +80,9 @@
             </div>
             <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
                 <div class="row pt-sm-4 pt-xs-3">
-                    <div class="col-md-6">
-                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Creative Manipulated & Correction  </b></h4>
-                        <p class="pb-sm-3 pb-xs-3">Designer Country manipulates the images with all diligent & experience 
+                    <div class="col-md-6 mobile-pt">
+                        <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4 mobile-pt"><b>Creative Manipulated & Correction  </b></h4>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">Designer Country manipulates the images with all diligent & experience 
                             besides editing them for other correction where we do morphing, blending, composting,
                             creative collage to the images</p>
                     </div>
@@ -102,8 +102,8 @@
 <div class="image_editing">
     <div class="container">
         <div class="imge-article">
-            <h1 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4">Creative Image Manipulation with Innovative Capability & Abstract Experience</h1>
-            <p>All Photoshop works are not like manipulated images because of admissibility for creative coordination for fashion,
+            <h1 class="mobile-pl mobile-pr pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4">Creative Image Manipulation with Innovative Capability & Abstract Experience</h1>
+            <p class="mobile-pl mobile-pr">All Photoshop works are not like manipulated images because of admissibility for creative coordination for fashion,
                 ecommerce, magazines. Manipulated photos proper using of all tools involving layer and color palate, lasso, 
                 move, and clone & also wand & brush will all Photoshop tools. Only ingenious & sharp-witted can manipulate.
                 <br><br>Social Media & Ecommerce Loading Connectivity to Photo? Manipulation Reference and correlation is 
@@ -113,8 +113,8 @@
                 these images easily can capture byre attention that grows traffic to your web.
 <br><br><br></p>
 
-            <h5 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Outsource Product Image & Headshot Photography Manipulation Editor–Designer Country</b></h5>
-            <p>Let us organize& produce remarkable shining digital photos for you to load on your page, use in marketing products & build 
+            <h5 class="mobile-padding pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Outsource Product Image & Headshot Photography Manipulation Editor–Designer Country</b></h5>
+            <p class="mobile-padding">Let us organize& produce remarkable shining digital photos for you to load on your page, use in marketing products & build 
                 relationships. Trading & product growth to fabricate virtually most of your photos, the Designer Country is responsible.
                 <br><br>All-up we own a strong & helpful perception of contemporaneous trends in manipulation service. Our creative manipulated 
                 experts with powerful & inventing mind to cover the manipulated area of your product or headshot photography. We also outsource 
