8ef6e5340936da6cb223c546ee0eea24a4e0cfc6
AzharRaihan <48883613+AzharRaihan@users.noreply.github.com>
2020-06-25 14:55:43 +0600

    color correction responsive complite

--- resources/views/frontEnd/pages/color_correction.blade.php
+++ resources/views/frontEnd/pages/color_correction.blade.php
@@ -10,7 +10,7 @@
 <!--==** Service Section Start **==-->
 <section id="service">
     <div class="container py-col-2 py-sm-3 py-md-4 py-lg-5 py-xl-5">
-        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3">
+        <div class="head pt-col-1 pt-sm-2 pt-md-2 pt-lg-3 pt-xl-3 mobile-pt">
             <h1 class="pb-col-2 pb-sm-3 pb-md-4 pb-lg-5 pb-xl-5 text-center">Color Correction Service</h1>
             <div class="box" style="box-shadow: 3px 3px 13px 2px rgba(0,0,0,.3);">
                 <img src="{{asset('assets/frontEnd/images/Service_All/Color_Correction_Service/Color_Correction_Service.png')}}"
@@ -27,7 +27,7 @@
 <!--==** Our Work Section Start **==-->
 <section id="out_work">
     <div class="container pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
-        <div class="head">
+        <div class="head mobile-pt">
             <h1 class="text-center text-uppercase">Our Work</h1>
         </div>
         <div class="box my-col-2 my-sm-3 my-md-4 my-lg-5 my-xl-5"
@@ -52,7 +52,7 @@
         <div class="design_body pb-col-2 pb-sm-3 pb-md-4 pb-lg-5 pb-xl-5">
             <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 mobile-pb">
                         <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Product Color Adjustment</b></h4>
                         <p class="pb-sm-3 pb-xs-3">To digitalize products for better quality color correction is a must to get correlative display & style. Before printing, this process also mandatory to let the products allow to be fulfilled.
                         </p>
@@ -63,11 +63,11 @@
                     </div>
                 </div>
             </div>
-            <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5">
-                <div class="row pt-sm-4 pt-xs-3">
+            <div class="single pt-col-2 pt-sm-3 pt-md-4 pt-lg-5 pt-xl-5 mobile-pt mobile-pb">
+                <div class="row pt-sm-4 pt-xs-3 mobile-pt mobile-pb">
                     <div class="col-md-6">
                         <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Portrait Photo Color</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">To be gorgeous, portrait photography also need to be reclosed. Via our color consulting you get shining & unstained ending on your portrait, they also develop contrast & resolution also.</p>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">To be gorgeous, portrait photography also need to be reclosed. Via our color consulting you get shining & unstained ending on your portrait, they also develop contrast & resolution also.</p>
                     </div>
                     <div class="col-md-6">
                         <img src="{{asset('assets/frontEnd/images/Service_All/Color_Correction_Service/Color_Correction_2.png')}}"
@@ -79,7 +79,7 @@
                 <div class="row pt-sm-4 pt-xs-3">
                     <div class="col-md-6">
                         <h4 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4"><b>Model Color Change</b></h4>
-                        <p class="pb-sm-3 pb-xs-3">We do model correction also using Photoshop & illustrator also Light room, we multi-color the model for suited & balanced color adjustment besides changing them using clipping path to multi-clothes.</p>
+                        <p class="pb-sm-3 pb-xs-3 mobile-pb">We do model correction also using Photoshop & illustrator also Light room, we multi-color the model for suited & balanced color adjustment besides changing them using clipping path to multi-clothes.</p>
                     </div>
                     <div class="col-md-6">
                         <img src="{{asset('assets/frontEnd/images/Service_All/Color_Correction_Service/Color_Correction_3.png')}}"
@@ -97,12 +97,12 @@
 <div class="image_editing">
     <div class="container">
         <div class="imge-article">
-            <h1 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4">Portrait, Model or Product Color Variants & Adjustment to Outsource Images from Asia</h1>
-            <p>Customarily throughout the photograph because of lack combination with brightness, situation, approach, and camera settings, the lifelike & instinctive color might be not perfect, subsequently by using the color adjustment process it may be controlled for naturalistic looking.
+            <h1 class="pb-col-2 pb-sm-2 pb-md-3 pb-lg-4 pb-xl-4 mobile-pl mobile-pr">Portrait, Model or Product Color Variants & Adjustment to Outsource Images from Asia</h1>
+            <p class="mobile-pl mobile-pr">Customarily throughout the photograph because of lack combination with brightness, situation, approach, and camera settings, the lifelike & instinctive color might be not perfect, subsequently by using the color adjustment process it may be controlled for naturalistic looking.
             When Europe & American people apply for outsourcing images, they are able to grow pressure low, money & time reserved. Essentially South Asian countries freelancers do these. Outsource Images Color Change to Variants & What We Supply 
             Our outsource group has got ready with thorough competence & experience in coloring products. In short supply photo adjustment, we do...
             <br><br></p>
-                <ul class="pl-col-2 pl-sm-3 pl-md-4 pl-lg-5 pl-xl-5 pb-3">
+                <ul class="pl-5 pl-col-2 pl-sm-3 pl-md-4 pl-lg-5 pl-xl-5 pb-3">
                     <li class="py-2">Edge Correction</li>
                     <li class="py-2">Eye Color Adjust</li>
                     <li class="py-2">Sun Effect Digitalize </li>
@@ -112,8 +112,8 @@
                     <li class="py-2">Removal Background & Clipping Path</li>
                     <li class="py-2">Skin, hair color Developed </li>
                 </ul>
-            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2"><b>Ecommerce Product & Portrait Model Color Solution- Designer Country </b></h5>
-            <p>How much concern yourself on the care of your products &Model, in our custody keeping them absolute & improved via color correction service, we let you get ultimate shot in low-priced outsource. <br>
+            <h5 class="pt-col-2 pt-sm-3 pt-md-3 pt-lg-4 pt-xl-4 pb-2 mobile-pl mobile-pr"><b>Ecommerce Product & Portrait Model Color Solution- Designer Country </b></h5>
+            <p class="mobile-pl mobile-pr">How much concern yourself on the care of your products &Model, in our custody keeping them absolute & improved via color correction service, we let you get ultimate shot in low-priced outsource. <br>
             We minutely mobilize ourselves by keeping our ears open to your recommendation so we effectively turn over the project completed. Pure looking products play as forerunners of an impression as they draw buyer awareness. <br>
             Besides coloring work, Designer Country lets clients hire itself out for Clipping or Cut Out, Image Processing, Shadow Making, Make Up solution & other Photoshop strategy.
             </p>
