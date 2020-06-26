5f55c379f82fb30ce0185533adec425abeeded50
AzharRaihan <48883613+AzharRaihan@users.noreply.github.com>
2020-06-27 00:36:36 +0600

    admin pannel correction complite

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -25,57 +25,23 @@
         <i class="app-menu__icon fas fa-id-badge"></i><span class="app-menu__label">Contact</span>
       </a>
     </li>
-
     <li>
       <a class="app-menu__item {{ Request::is('admin/quote') ? "active" : ""}}"  href="{{route('quote')}}">
         <i class="app-menu__icon fas fa-id-badge"></i><span class="app-menu__label">Quote</span>
       </a>
-    </li>
-
-
-    <li>
-      <a class="app-menu__item" href="charts.html"><i class="app-menu__icon  fas fa-chart-pie"></i><span class="app-menu__label">Charts</span></a>
-    </li>
-    <li class="treeview">
-      <a class="app-menu__item" href="#" data-toggle="treeview">
-        <i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Forms</span><i class="treeview-indicator fa fa-angle-right"></i>
-      </a>
-      <ul class="treeview-menu">
-        <li>
-          <a class="treeview-item" href="form-components.html">
-            <i class="icon fa fa-circle-o"></i> Form Components</a>
-        </li>
-        <li>
-          <a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom Components
-          </a>
-        </li>
-        <li>
-          <a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form Samples
-          </a>
-        </li>
-        <li>
-          <a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form
-              Notifications
-          </a>
-        </li>
-      </ul>
     </li>
     <li class="treeview">
       <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span   class="app-menu__label">Tables</span><i class="treeview-indicator fa fa-angle-right"></i>
       </a>
       <ul class="treeview-menu">
         <li>
-          <a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a>
+          <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Basic Tables</a>
         </li>
         <li>
-          <a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data
+          <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Data
           Tables</a>
         </li>
       </ul>
-    </li>
-    <li>
-      <a class="app-menu__item" href="docs.html"><i class="app-menu__icon far fa-file-alt"></i><span
-      class="app-menu__label">Docs</span></a>
     </li>
   </ul>
 </aside>
